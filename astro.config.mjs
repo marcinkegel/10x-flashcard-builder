204371ccc178fadd283cf7b04324208490e77cc3
marcinkegel <61429083+marcinkegel@users.noreply.github.com>
2026-02-07 17:00:52 +0100

    Revert "feature| cicd build and deployment action added"

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +14,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
